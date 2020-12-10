e62bcddd49008d898a5f988dc73c420e14bcfe85
HOMDOELUNA <1402692684@qq.com>
2020-12-10 17:17:06 +0800

    添加了多项式,和一些基本函数

--- 大数联机实践/主程序.cpp
+++ 大数联机实践/主程序.cpp
@@ -1,5 +1,22 @@
 #include<iostream>
 
 #include"Polynomial.h"
-
+using namespace std;
 //HOMODELUNA@outlook.com
+
+
+
+
+int main() {
+	Polynomial a;
+	a.degree = 2;
+	a.c.push_back(0);a.c.push_back(0);a.c.push_back(1);
+	a << 2;
+	cout << a(2) <<'\n';
+	a >> 2;
+	cout << a(2) << '\n';
+	a = a.der(2);
+	cout << a(2) << '\n';
+	a = a.inte(2);
+	cout << a(2) << '\n';
+}
